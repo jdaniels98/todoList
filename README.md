75e5286a538e8993bf01f9c5f65ac1167ff3a7d3
jdaniels98 <jackdanielswork98@gmail.com>
2022-09-30 17:24:55 +0100

    footer text changed and finalised

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-TBD
+This is my To-Do List for the CodeNation Week 5 React.js challenge.
+
+These instructions are on the site itself but for the sake of thoroughness, here is the functionality of this project:
+
+There are two lists, TO-DO and COMPLETE. Using the ENTER key or the ADD ITEM button after entering text into the input field will add the item to the TO-DO list.
+
+Clicking the text of an entry on the TO-DO list will cause it to strike through the text, clicking again after the fact will remove the line.
+
+However, double-clicking the text of an entry on the TO-DO list will remove that item entirely.
+
+The EDIT button will allow you to edit the text of the selected entry, and clicking SAVE will apply it.
+
+The COMPLETE button will transfer an entry over to the COMPLETE list.
+
+Double-clicking an entry on the COMPLETE list will remove that entry entirely.
+
+The CLEAR LIST button will clear the respective list.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,19 +8,25 @@ const Footer = () => {
             <br></br>
             "Add Item" / Enter Key: add inputted text to list
             <br></br>
-            "Clear List": removes all items from TO-DO LIST
+            "Clear List": removes all items from TO-DO list
             <br></br>
             On Click: strike through an item
             <br></br>
-            On Double Click: remove a specific item from TO-DO LIST</p>
+            On Double Click: remove a specific item from TO-DO list
+            <br></br>
+            "Edit": allows for editing the text of that entry
+            <br></br>
+            "Save": applies any changes made via editing
+            <br></br>
+            "Complete": moves that entry from the TO-DO list to the COMPLETE list</p>
         </div>
             <p id="footertext2">
             <i id="footertitle">COMPLETE</i>
             <br></br>
             <br></br>
-            On Click: remove a specific item from COMPLETE LIST
+            On Click: remove a specific item from COMPLETE list
             <br></br>
-            "Clear List": removes all items from COMPLETE LIST
+            "Clear List": removes all items from COMPLETE list
             </p>
         </footer>
     )
